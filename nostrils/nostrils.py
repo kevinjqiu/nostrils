eb297111060dcb54503d77adf55c2cf02f3c74d9
Kevin Qiu <kevin@freshbooks.com>
2011-11-09 22:13:00 -0500

    pynotify works

--- nostrils/nostrils.py
+++ nostrils/nostrils.py
@@ -1,5 +1,7 @@
-from nose.plugins import Plugin
 import logging
+import httplib
+import json
+from nose.plugins import Plugin
 
 log = logging.getLogger(__name__)
 
@@ -8,9 +10,14 @@ class Nostrils(Plugin):
 
     def configure(self, options, conf):
         super(Nostrils, self).configure(options, conf)
+        self.nostrils_server = options.nostrils_server
 
     def add_options(self, parser, env=None):
         super(Nostrils, self).add_options(parser, env)
+        parser.add_option('-n', '--nostrils-server', dest='nostrils_server', help='nostrils server location')
 
     def addFailure(self, test, err, *args):
-        pass
+        conn = httplib.HTTPConnection(self.nostrils_server)
+        conn.request("POST", "/fail", json.dumps({'test':str(test)}))
+        response = conn.getresponse()
+        log.info(response)
--- nostrils/server.py
+++ nostrils/server.py
@@ -1,14 +1,22 @@
+import logging
 from wsgiref.simple_server import make_server
+from pynotify import Notification
+
+log = logging.getLogger(__name__)
+
+def handle(type_, response, environ):
+    body = environ['wsgi.input'].read(int(environ['CONTENT_LENGTH']))
+    n = Notification('Error', body)
+    n.show()
+    log.info(body)
+    response('200 OK', [('Content-Type', 'text/plain')])
+    return ['%s notified' % type_]
 
 def nostril_server(environ, start_response):
     if environ['PATH_INFO'] == '/fail':
-        body = environ['wsgi.input'].read(int(environ['CONTENT_LENGTH']))
-        start_response('200 OK', [('Content-Type', 'text/plain')])
-        return 'failure notified. %s' % body
+        handle('failure', start_response, environ)
     elif environ['PATH_INFO'] == '/error':
-        body = environ['wsgi.input'].read(int(environ['CONTENT_LENGTH']))
-        start_response('200 OK', [('Content-Type', 'text/plain')])
-        return 'error notified'
+        handle('error', start_response, environ)
     else:
         start_response('404 Not Found', [('Content-Type', 'text/plain')])
         return ''

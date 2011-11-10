19103a26ed1fa24217130fdb5c21059a92e6cef6
Kevin Qiu <kevin@freshbooks.com>
2011-11-10 00:08:13 -0500

    a naon framework for handling route/method and their handlers

--- nostrils/server.py
+++ nostrils/server.py
@@ -1,25 +1,44 @@
 import logging
 from wsgiref.simple_server import make_server
 from pynotify import Notification
+from functools import wraps
 
 log = logging.getLogger(__name__)
 
-def handle(type_, response, environ):
+ROUTE_MAP = {}
+
+def handles(route):
+    def decorator(f):
+        ROUTE_MAP[route] = f
+        @wraps(f)
+        def wrapper(environ, response):
+            return f(environ, response)
+
+    return decorator
+
+@handles(('POST', '/fail'))
+def on_fail(environ, response):
     body = environ['wsgi.input'].read(int(environ['CONTENT_LENGTH']))
-    n = Notification('Error', body)
-    n.show()
-    log.info(body)
-    response('200 OK', [('Content-Type', 'text/plain')])
-    return ['%s notified' % type_]
+    Notification('Error', body).show()
+    response('200 OK', body=['Error notified'])
+
+@handles(('GET', '/ping'))
+def ping(environ, response):
+    return response('200 OK', body=['pong'])
 
 def nostril_server(environ, start_response):
-    if environ['PATH_INFO'] == '/fail':
-        handle('failure', start_response, environ)
-    elif environ['PATH_INFO'] == '/error':
-        handle('error', start_response, environ)
+    def response(status, content_type='text/plain', body=''):
+        start_response(status, [('Content-Type', content_type)])
+        return body
+
+    path = environ['PATH_INFO']
+    method = environ['REQUEST_METHOD']
+
+    if (method, path) in ROUTE_MAP.keys():
+        handler = ROUTE_MAP[(method, path)]
+        return handler(environ, response)
     else:
-        start_response('404 Not Found', [('Content-Type', 'text/plain')])
-        return ''
+        response('404 Not Found')
 
 if __name__ == '__main__':
     # for local testing

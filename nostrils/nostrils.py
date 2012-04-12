6d73f7858655220741ed15af81069466dac1c41b
Kevin Qiu <kevin@freshbooks.com>
2012-04-11 23:07:31 -0400

    wip

--- nostrils/nostrils.py
+++ nostrils/nostrils.py
@@ -1,7 +1,4 @@
 import logging
-import httplib
-import traceback
-import json
 from nose.plugins import Plugin
 
 log = logging.getLogger(__name__)
@@ -9,25 +6,23 @@ log = logging.getLogger(__name__)
 class Nostrils(Plugin):
     name = 'nostrils'
 
-    def _send(self, path, info):
-        conn = httplib.HTTPConnection(self.nostrils_server)
-        conn.request("POST", path, json.dumps(info))
-        response = conn.getresponse()
-        log.info(response)
-
     def configure(self, options, conf):
         super(Nostrils, self).configure(options, conf)
-        self.nostrils_server = options.nostrils_server
+        if self.enabled:
+            try:
+                import coverage
+            except ImportError:
+                log.error('Coverage not available:unable to import coverage module')
+                self.enabled = False
+                return
+            self.conf = conf
+
+            if self.enabled:
+                self.coverage_instance = coverage.coverage(auto_data=False)
 
     def add_options(self, parser, env=None):
         super(Nostrils, self).add_options(parser, env)
-        parser.add_option('-n', '--nostrils-server', dest='nostrils_server', help='nostrils server location')
 
-    def addFailure(self, test, err, *args):
-        _, exception, tb = err
-        info = {
-            'type':'Failure',
-            'test_name':str(test),
-            'top_stackframe':traceback.extract_tb(tb)[-1]
-        }
-        self._send('/fail', info)
+    def begin(self):
+        self.coverage_instance.load()
+        self.coverage_instance.start()

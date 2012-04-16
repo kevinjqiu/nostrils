daeec73ce342920bacd87d133e86b4e2f18e3cb7
Kevin Qiu <kevin@freshbooks.com>
2012-04-16 16:57:37 -0400

    added yield test.

--- test/test_worker.py
+++ test/test_worker.py
@@ -16,3 +16,12 @@ class TestFoo(object):
 
     def test_multi(self):
         assert 55 == worker.multi(5, 11)
+
+def test_with_yield():
+
+    def assert_arithmetic(op, expected, *args):
+        assert expected == apply(op, args)
+
+    yield assert_arithmetic, worker.add, 5, 2, 3
+    yield assert_arithmetic, worker.subtract, 1, 3, 2
+    yield assert_arithmetic, worker.multi, 99, 11, 9

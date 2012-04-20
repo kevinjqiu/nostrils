8c978ef51d4775c6b51fd79756438d3bd9b7cbfe
Kevin Qiu <kevin@freshbooks.com>
2012-04-20 00:47:00 -0400

    sample test file for pyinotify experiments

---
+++ sample.py
@@ -0,0 +1,15 @@
+from pyinotify import WatchManager
+from pyinotify import Notifier
+from pyinotify import ALL_EVENTS
+
+wm = WatchManager()
+
+def cb(s):
+    pass
+
+def process_event(event, *args, **kwargs):
+    print event
+
+wm.add_watch('.', mask=ALL_EVENTS)
+notifier = Notifier(wm, default_proc_fun=process_event)
+notifier.loop(callback=cb)
